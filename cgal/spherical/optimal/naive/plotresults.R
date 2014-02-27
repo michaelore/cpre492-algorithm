9a443425967e8c496648abefea1ae886a93a03f6
Michael Ore <selbram1993@gmail.com>
2014-02-27 04:54:14 -0600

    Set up plotresults.R as a unix script

--- cgal/spherical/optimal/naive/plotresults.R
+++ cgal/spherical/optimal/naive/plotresults.R
@@ -1,3 +1,4 @@
+#!/usr/bin/env Rscript
 require(plotrix)
 circledata <- read.table('00032results.txt')
 ylow = min(c(circledata$V2, circledata$V4, circledata$V6))
@@ -7,16 +8,12 @@ xlow = min(c(circledata$V1, circledata$V3, circledata$V5))
 xhig = xlow + yhig-ylow
 plot(circledata$V7, circledata$V8, xlim=c(xlow, xhig), ylim=c(ylow, yhig),  pch=19, cex=0.4, col="blue")
 par(new=T)
-Sys.sleep(1)
 plot(circledata$V1, circledata$V2, xlim=c(xlow, xhig), ylim=c(ylow, yhig),  pch=19, cex=0.5, col="red")
 par(new=T)
-Sys.sleep(1)
 plot(circledata$V3, circledata$V4, xlim=c(xlow, xhig), ylim=c(ylow, yhig),  pch=19, cex=0.5, col="red")
 par(new=T)
-Sys.sleep(1)
 plot(circledata$V5, circledata$V6, xlim=c(xlow, xhig), ylim=c(ylow, yhig),  pch=19, cex=0.5, col="red")
 par(new=T)
-Sys.sleep(1)
 for (i in 1:length(circledata$V9)) {
     draw.circle(circledata$V7[i], circledata$V8[i], circledata$V9[i])
 }
