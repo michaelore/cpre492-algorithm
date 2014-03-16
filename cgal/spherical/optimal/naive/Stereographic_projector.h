f4aa06e440392ba0ab6158ce878dedeb4e971bdc
Michael Ore <selbram1993@gmail.com>
2014-03-15 22:45:25 -0500

    Fixed Stereographic_projector to be stereographic instead of gnomonic

--- cgal/spherical/optimal/naive/Stereographic_projector.h
+++ cgal/spherical/optimal/naive/Stereographic_projector.h
@@ -15,19 +15,20 @@ namespace CGAL {
 
     public:
         Stereographic_projector(Sphere_3 &s, Point_3 &p) :
-            sphere(s), point(p) {
-                plane = Plane_3(point, sphere.center()-point);
+            sphere(s) {
+                antipodal = sphere.center()-(p-sphere.center());
+                plane = Plane_3(p, sphere.center()-p);
             }
 
         Point_2 operator()(Point_3 &p3) { return projection(p3); }
 
     private:
-        Point_3 point;
+        Point_3 antipodal;
         Plane_3 plane;
         Sphere_3 sphere;
 
         Point_2 projection(Point_3 &p3) {
-            Line_3 line(p3, sphere.center());
+            Line_3 line(antipodal, p3);
             Object projection = intersection(line, plane);
             return plane.to_2d(object_cast<Point_3>(projection));
         }
