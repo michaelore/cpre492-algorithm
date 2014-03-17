8dd41ffa7e149e1680218c839242a8e21e9b19ed
Michael Ore <selbram1993@gmail.com>
2014-03-16 21:03:18 -0500

    refactored Angle_range.h

--- cgal/spherical/optimal/naive/Angle_range.h
+++ cgal/spherical/optimal/naive/Angle_range.h
@@ -21,49 +21,41 @@ namespace CGAL {
     public:
         Angle_range(Circle_3 &c) : circle(c) {
                 Arc_3 full_range = Arc_3(c);
-                range = CGAL::make_object(full_range);
-                empty_range = false;
+                available_sets.push_back(make_object(full_range));
         }
 
         void restrict_with_point(Point_3 &p) {
-            if (!empty_range) {
-                Arc_3 opp_arc = get_opposing_arc(circle, p);
-                std::vector<CGAL::Object> restricted;
-                if (const Arc_3* casted_range = CGAL::object_cast<Arc_3>(&range)) {
-                    CGAL::intersection(*casted_range, opp_arc, back_inserter(restricted));
-                    if (restricted.size() > 1) {
-                        std::cerr << "Something is very wrong!2" << std::endl;
-                    } else if (restricted.size() == 1) {
-                        range = restricted[0];
-                    } else {
-                        empty_range = true;
+            Arc_3 opp_arc = get_opposing_arc(circle, p);
+            std::vector<Object> restricted;
+            std::pair<Arc_Point_3, unsigned> app;
+            Arc_3 arc;
+            for (int i = 0; i < available_sets.size(); i++) {
+                if (assign(app, available_sets[i])) {
+                    if (K().has_on_3_object()(opp_arc, app.first)) {
+                        restricted.push_back(make_object(app.first));
                     }
-                } else if (const std::pair<Arc_Point_3, unsigned>* casted_range_with_mult = CGAL::object_cast<std::pair<Arc_Point_3, unsigned> >(&range)) {
-                    if (!K().has_on_3_object()(opp_arc, casted_range_with_mult->first)) {
-                        empty_range = true;
-                    }
-                } else {
-                    //
+                } else if (assign(arc, available_sets[i])) {
+                    intersection(opp_arc, arc, back_inserter(restricted));
                 }
             }
+            available_sets = restricted;
         }
 
         bool is_empty() {
-            return empty_range;
+            return available_sets.empty();
         }
 
     private:
         Circle_3 circle;
-        Object range;
-        bool empty_range;
+        std::vector<Object> available_sets;
 
         Arc_3 get_opposing_arc(Circle_3 circle, Point_3 point) {
             Plane_3 orthogonal_plane(circle.center(), point-circle.center());
 
-            std::vector<CGAL::Object> orthogonal_points;
-            CGAL::intersection(circle, orthogonal_plane, back_inserter(orthogonal_points));
-            Arc_Point_3 orthogonal_one = CGAL::object_cast<std::pair<Arc_Point_3, unsigned> >(orthogonal_points[0]).first;
-            Arc_Point_3 orthogonal_two = CGAL::object_cast<std::pair<Arc_Point_3, unsigned> >(orthogonal_points[1]).first;
+            std::vector<Object> orthogonal_points;
+            intersection(circle, orthogonal_plane, back_inserter(orthogonal_points));
+            Arc_Point_3 orthogonal_one = object_cast<std::pair<Arc_Point_3, unsigned> >(orthogonal_points[0]).first;
+            Arc_Point_3 orthogonal_two = object_cast<std::pair<Arc_Point_3, unsigned> >(orthogonal_points[1]).first;
 
             Arc_3 arc_one(circle, orthogonal_one, orthogonal_two);
             Arc_3 arc_two(circle, orthogonal_two, orthogonal_one);
