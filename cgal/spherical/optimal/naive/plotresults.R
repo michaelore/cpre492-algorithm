63f23c5cef87847b2af0c25908027a32905d7ff5
Michael Ore <selbram1993@gmail.com>
2014-03-18 09:43:36 -0500

    updated plotresults.R

--- cgal/spherical/optimal/naive/plotresults.R
+++ cgal/spherical/optimal/naive/plotresults.R
@@ -3,26 +3,45 @@ require(plotrix)
 
 args = commandArgs(trailingOnly = TRUE)
 
-circledata <- read.table(args[1])
-xmin = min(c(circledata$V1, circledata$V3, circledata$V5))
-xmax = max(c(circledata$V1, circledata$V3, circledata$V5))
-ymin = min(c(circledata$V2, circledata$V4, circledata$V6))
-ymax = max(c(circledata$V2, circledata$V4, circledata$V6))
-xlow = xmin - 0*(xmax-xmin)
-xhig = xmax + 0*(xmax-xmin)
-ylow = ymin - 0*(ymax-ymin)
-yhig = ymax + 0*(ymax-ymin)
-plot(circledata$V7, circledata$V8, xlim=c(xlow, xhig), ylim=c(ylow, yhig),  pch=19, cex=0.4, col="blue", asp=1)
-par(new=T)
-plot(circledata$V1, circledata$V2, xlim=c(xlow, xhig), ylim=c(ylow, yhig),  pch=19, cex=0.5, col="red", asp=1)
-par(new=T)
-plot(circledata$V3, circledata$V4, xlim=c(xlow, xhig), ylim=c(ylow, yhig),  pch=19, cex=0.5, col="red", asp=1)
-par(new=T)
-plot(circledata$V5, circledata$V6, xlim=c(xlow, xhig), ylim=c(ylow, yhig),  pch=19, cex=0.5, col="red", asp=1)
-par(new=T)
-for (i in 1:length(circledata$V9)) {
-    draw.circle(circledata$V7[i], circledata$V8[i], circledata$V9[i])
+tau = 6.283185
+
+stereo = function(lat, lon, clat, clon) {
+    phi = lat*tau/360
+    lam = lon*tau/360
+    phi1 = clat*tau/360
+    lam0 = clon*tau/360
+    r = 6371009
+    k = 2*r/(1 + sin(phi1)*sin(phi)+cos(phi1)*cos(phi)*cos(lam-lam0))
+    x = k*cos(phi)*sin(lam-lam0)
+    y = k*(cos(phi1)*sin(phi)-sin(phi1)*cos(phi)*cos(lam-lam0))
+    return(data.frame(x, y))
 }
-verifyradius = sqrt((circledata$V5-circledata$V7)**2+(circledata$V6-circledata$V8)**2)
-radiusdiff = abs(circledata$V9-verifyradius)
-summary(radiusdiff)
+
+pointdata = read.table(args[1])
+center = tail(pointdata, 1)
+stereodata = stereo(pointdata$V1, pointdata$V2, center$V1, center$V2)
+circledata = read.table(args[2])
+#xmin = min(c(circledata$V1, circledata$V3, stereodata[[1]]))
+#xmax = max(c(circledata$V1, circledata$V3, stereodata[[1]]))
+#ymin = min(c(circledata$V2, circledata$V4, stereodata[[2]]))
+#ymax = max(c(circledata$V2, circledata$V4, stereodata[[2]]))
+xmin = min(c(circledata$V3, stereodata[[1]]))
+xmax = max(c(circledata$V3, stereodata[[1]]))
+ymin = min(c(circledata$V4, stereodata[[2]]))
+ymax = max(c(circledata$V4, stereodata[[2]]))
+xlow = xmin
+xhig = xmax
+ylow = ymin
+yhig = ymax
+plot(stereodata[[1]], stereodata[[2]], xlim=c(xlow, xhig), ylim=c(ylow, yhig),  pch=19, cex=0.5, col="red", asp=1)
+par(new=T)
+plot(circledata$V1, circledata$V2, xlim=c(xlow, xhig), ylim=c(ylow, yhig),  pch=19, cex=0.4, col="green", asp=1)
+par(new=T)
+plot(circledata$V3, circledata$V4, xlim=c(xlow, xhig), ylim=c(ylow, yhig),  pch=19, cex=0.4, col="blue", asp=1)
+par(new=T)
+for (i in 1:length(circledata$V5)) {
+    draw.circle(circledata$V3[i], circledata$V4[i], circledata$V5[i])
+}
+
+summary(stereodata)
+summary(circledata)
